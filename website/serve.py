e32a86165bff6290c7728fafbcebac058abcac91
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-05 00:31:56 -0400

    Update serve.py

--- website/serve.py
+++ website/serve.py
@@ -1,52 +1,52 @@
+import socket
 import http.server
 import socketserver
 import json
 import os
-from threading import Lock
 
 PORT = 58541
-data_lock = Lock()
+
+
 
 def load_data(default_data):
-    with data_lock:  # Ensure thread-safe file access
-        if not os.path.isfile('data.json'):
-            with open('data.json', 'w') as f:
-                json.dump(default_data, f)
+    if not os.path.isfile('data.json'):
+        with open('data.json', 'w') as f:
+            json.dump(default_data, f)
+        return default_data
+    else:
+        try:
+            with open('data.json', 'r') as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            # Handle the case where the JSON is not able to be decoded
+            print("Error reading the JSON file: JSONDecodeError")
             return default_data
-        else:
-            try:
-                with open('data.json', 'r') as f:
-                    return json.load(f)
-            except json.JSONDecodeError:
-                # Handle the case where the JSON file is corrupt or empty
-                with open('data.json', 'w') as f:
-                    json.dump(default_data, f)
-                return default_data
 
-# Define the default data structure at the top of your script
+# Usage
 default_data_structure = {
     'total_loads': 0,
-    'unique_visitors': []
+    'unique_visitors': set()
 }
+data = load_data(default_data_structure)
 
+
+# Save the current counts to a file
 def save_data(data):
-    with data_lock:  # Ensure thread-safe file access
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=4)
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
+# Update the counts and save to the file
 def update_data(ip):
-    data = load_data(default_data_structure)
+    data = load_data()
     data['total_loads'] += 1
-    if ip not in data['unique_visitors']:
-        data['unique_visitors'].append(ip)
+    data['unique_ips'].add(ip)
     save_data(data)
 
 class LoggingHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.server_version = "MyHTTP/"  # Customize the server name if desired
-        self.sys_version = ""  # Hide the Python version
         # Update data with the IP of the client making the request
         update_data(self.client_address[0])
+        
         # Call superclass method to actually handle the request
         super().do_GET()
 
@@ -57,6 +57,5 @@ class LoggingHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 Handler = LoggingHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
