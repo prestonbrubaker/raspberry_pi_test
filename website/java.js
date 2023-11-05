ddb46ad1bb1c2dd7b76dc1fdb534bd95bb08de94
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-05 00:36:16 -0400

    Update java.js

--- website/java.js
+++ website/java.js
@@ -1,17 +1,13 @@
-// This function fetches the data from the data.json file
 function fetchData() {
   fetch('data.json')
-    .then(response => response.json()) // Parse the JSON from the response
+    .then(response => response.json())
     .then(data => {
-      // Update the DOM elements with the data
       document.getElementById('total-loads').textContent = data.total_loads;
       document.getElementById('unique-devices').textContent = data.unique_visitors.length;
     })
     .catch(error => {
-      // Handle any errors here
       console.error('Error fetching data:', error);
     });
 }
 
-// Call the function when the script loads
 fetchData();
