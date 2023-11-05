e9b400ef697b64a1c6d9623544c64543a57d786a
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-04 20:30:33 -0400

    Update serve.py

--- website/serve.py
+++ website/serve.py
@@ -2,7 +2,16 @@ import http.server
 import socketserver
 
 PORT = 58541
-Handler = http.server.SimpleHTTPRequestHandler
+
+class LoggingHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        with open("log.txt", "a") as file:
+            file.write("%s - - [%s] %s\n" %
+                       (self.client_address[0],
+                        self.log_date_time_string(),
+                        format%args))
+
+Handler = LoggingHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
